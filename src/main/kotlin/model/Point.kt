f2d17c462de49836ef0668f0fa7cd4151566a22e
tsteffek <thomas.steffek@beuth-hochschule.de>
2019-11-14 11:12:00 +0100

    fixed tests; updated version to 0.1

--- src/main/kotlin/model/Point.kt
+++ src/main/kotlin/model/Point.kt
@@ -20,4 +20,17 @@ data class Point(val x: Double, val y: Double, val quality: Int) {
     }
 
     fun toTSVString() = "$x\t$y"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Point
+
+        if (quality != other.quality) return false
+        if (other.x - 1e-8 > x || x > other.x + 1e-8) return false
+        if (other.y - 1e-8 > y || y > other.y + 1e-8) return false
+
+        return true
+    }
 }
--- src/test/kotlin/model/Scan2DTest.kt
+++ src/test/kotlin/model/Scan2DTest.kt
@@ -1,70 +1,127 @@
 package model
 
 import io.kotlintest.TestCase
+import io.kotlintest.matchers.asClue
 import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.matchers.collections.shouldNotContain
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.tables.*
 import mu.KotlinLogging
 
 private val logger by lazy { KotlinLogging.logger {} }
 
-class Scan2DTest : StringSpec() {
-
-    private lateinit var tsv: String
-    private lateinit var tsvInc: String
-    private lateinit var testScanner: Scanner
-    private lateinit var testScannerInc: Scanner
-    private lateinit var targetPointCloud: Table1<Point>
-
-    override fun beforeTest(testCase: TestCase) {
-        super.beforeTest(testCase)
-
-        testScanner = Scanner("TestScanner-Total", false, 45.0, true)
-        tsv = """
-            id	step size	distance	quality
-            1	0	1	54
-            2	1	1.41421356	58
-            13	2	1	62
-        """.trimIndent()
-
-        testScannerInc = Scanner("TestScanner-Inc", true, 45.0, true)
-        tsvInc = """
-            id	step size	distance	quality
-            1	0	1	54
-            2	1	1.41421356	58
-            13	1	1	62
-        """.trimIndent()
-
-        targetPointCloud = table(
-            headers("Point"),
-            row(Point(0.0, 1.0, 54)),
-            row(Point(1.0, 1.0, 58)),
-            row(Point(1.0, 0.0, 62))
-        )
-    }
+class Scan2DTest : FreeSpec() {
 
     init {
-        "csv should load into rawData" {
-            val scan = Scan2D.fromTSV(tsv, testScanner)
-            scan.rawData.size shouldBe 3
-            scan.rawData.first() shouldBe Scan2D.ScanData(1, 0.0, 1.0, 54)
-            scan.rawData.last() shouldBe Scan2D.ScanData(13, 1.0, 1.0, 62)
-        }
+        "companion object" - {
 
-        "pointCloud should be computed from total values" {
-            val scan = Scan2D.fromTSV(tsv, testScanner)
-            scan.pointCloud.size shouldBe 3
-            forAll(targetPointCloud) {
-                scan.pointCloud shouldContain it
+            val testScanner = Scanner("TestScanner-Total", false, 45.0, true)
+            val tsv = """
+                id	step size	distance	quality
+                1	0	1	54
+                2	1	1.41421356	58
+                13	2	1	62
+            """.trimIndent()
+
+            "fromTSV should load into rawData in correct order" {
+                val scan = Scan2D.fromTSV(tsv, testScanner)
+                scan.rawData.size shouldBe 3
+                scan.rawData.last() shouldBe Scan2D.ScanData(13, 2.0, 1.0, 62)
+                scan.rawData.first() shouldBe Scan2D.ScanData(1, 0.0, 1.0, 54)
             }
         }
 
-        "pointCloud should be computed from incremental values" {
-            val scan = Scan2D.fromTSV(tsvInc, testScannerInc)
-            scan.pointCloud.size shouldBe 3
-            forAll(targetPointCloud) {
-                scan.pointCloud shouldContain it
+        "pointCloud" - {
+            "from total values" - {
+
+                val testScanner = Scanner("TestScanner-Total", false, 45.0, true)
+                val tsv = """
+                    id	step size	distance	quality
+                    1	0	1	54
+                    2	1	1.41421356	58
+                    13	2	1	62
+                """.trimIndent()
+                val tsvReverse = """
+                    id	step size	distance	quality
+                    1	0	1	54
+                    2	1	1.41421356	58
+                    13	2	1	62
+                """.trimIndent()
+
+                val targetPointCloud = table(
+                    headers("Point"),
+                    row(Point(0.0, 1.0, 54)),
+                    row(Point(1.0, 1.0, 58)),
+                    row(Point(1.0, 0.0, 62))
+                )
+
+                "should be computed" {
+                    val scan = Scan2D.fromTSV(tsv, testScanner)
+                    scan.pointCloud.size shouldBe 3
+                    scan.pointCloud.asClue {
+                        forAll(targetPointCloud) {
+                            scan.pointCloud shouldContain it
+                        }
+                    }
+                }
+
+                "should be computed, even reverse" {
+                    val scan = Scan2D.fromTSV(tsvReverse, testScanner)
+                    scan.pointCloud.size shouldBe 3
+                    scan.pointCloud.asClue {
+                        forAll(targetPointCloud) {
+                            scan.pointCloud shouldContain it
+                        }
+                    }
+                }
+            }
+
+            "from incremental values" - {
+
+                val testScannerInc = Scanner("TestScanner-Inc", true, 45.0, true)
+
+                val tsvInc = """
+                    id	step size	distance	quality
+                    13	0	1	54
+                    2	1	1.41421356	58
+                    1	1	1	62
+                """.trimIndent()
+
+                val tsvIncReverse = """
+                    id	step size	distance	quality
+                    13	0	1	54
+                    2	1	1.41421356	58
+                    1	1	1	62
+                """.trimIndent()
+
+                val targetPointCloud = table(
+                    headers("Point"),
+                    row(Point(0.0, 1.0, 54)),
+                    row(Point(1.0, 1.0, 58)),
+                    row(Point(1.0, 0.0, 62))
+                )
+
+                "should be computed" {
+                    val scan = Scan2D.fromTSV(tsvInc, testScannerInc)
+                    scan.pointCloud.size shouldBe 3
+                    scan.pointCloud.asClue {
+                        forAll(targetPointCloud) {
+                            scan.pointCloud shouldContain it
+                        }
+                    }
+                }
+
+                "should be computed, even reverse" {
+                    val scan = Scan2D.fromTSV(tsvIncReverse, testScannerInc)
+                    scan.pointCloud.size shouldBe 3
+                    scan.pointCloud.asClue {
+                        forAll(targetPointCloud) {
+                            scan.pointCloud shouldContain it
+                        }
+                    }
+                }
             }
         }
     }
