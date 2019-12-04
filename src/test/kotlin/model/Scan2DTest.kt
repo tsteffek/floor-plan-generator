a89d6c9e8888b85645dc59d2a3989104489dfa52
tsteffek <thomas.steffek@beuth-hochschule.de>
2019-12-04 17:30:04 +0100

    fixed point and scan tests

--- src/test/kotlin/model/Scan2DTest.kt
+++ src/test/kotlin/model/Scan2DTest.kt
@@ -1,7 +1,9 @@
 package model
 
+import io.kotlintest.assertSoftly
 import io.kotlintest.matchers.asClue
 import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.FreeSpec
 import io.kotlintest.tables.*
@@ -11,60 +13,43 @@ import kotlin.math.sqrt
 
 private val logger by lazy { KotlinLogging.logger {} }
 
-class Scan2DTest : FreeSpec() {
-
-    init {
-        "companion object" - {
-
-            val testScanner = Scanner("TestScanner-Total", false, 45.0, true, 100)
-            val tsv = """
-                id	step size	distance	quality
-                1	0	1	54
-                2	1	1.41421356	58
-                13	2	1	62
-            """.trimIndent()
-        }
-
-        "pointCloud" - {
+class Scan2DTest : FreeSpec({
+    "companion object" - {
+        "fromTSV" - {
             "from total values" - {
-
-                val testScanner = Scanner("TestScanner-Total", false, 45.0, true, 100)
+                val testScanner = Scanner("TestScanner-Total", false, 45.0, false, 100)
                 val tsv = """
                     id	step size	distance	quality
-                    1	0	1	54
-                    2	1	1.41421356	58
-                    13	2	1	62
+                    1	0	1	1
+                    2	1	1.41421356	2
+                    13	2	1	3
                 """.trimIndent()
                 val tsvReverse = """
                     id	step size	distance	quality
-                    1	0	1	54
-                    2	1	1.41421356	58
-                    13	2	1	62
+                    1	0	1	1
+                    2	1	1.41421356	2
+                    13	2	1	3
                 """.trimIndent()
 
                 val targetPointCloud = table(
                     headers("Point"),
-                    row(Point(1.0, 0.0, 54)),
-                    row(Point(sqrt(2.0), 45.0, 58)),
-                    row(Point(1.0, -90.0, 62))
+                    row(Point(1.0, 0.0, 1)),
+                    row(Point(sqrt(2.0), 45.0, 2)),
+                    row(Point(1.0, 90.0, 3))
                 )
 
-                "should be computed" {
-                    val scan = Scan2D.fromTSV(tsv, testScanner)
-                    scan.pointCloud.size shouldBe 3
-                    scan.pointCloud.asClue {
-                        forAll(targetPointCloud) {
-                            scan.pointCloud shouldContain it
-                        }
-                    }
-                }
-
-                "should be computed, even reverse" {
-                    val scan = Scan2D.fromTSV(tsvReverse, testScanner)
-                    scan.pointCloud.size shouldBe 3
-                    scan.pointCloud.asClue {
-                        forAll(targetPointCloud) {
-                            scan.pointCloud shouldContain it
+                listOf(
+                    row("normal tsv", Scan2D.fromTSV(tsv, testScanner)),
+                    row("reverse tsv", Scan2D.fromTSV(tsvReverse, testScanner))
+                ).map { (method, scan) ->
+                    "should create the pointCloud correctly from $method" {
+                        assertSoftly {
+                            scan.pointCloud.size shouldBe 3
+                            scan.pointCloud.asClue {
+                                forAll(targetPointCloud) {
+                                    scan.pointCloud shouldContain it
+                                }
+                            }
                         }
                     }
                 }
@@ -72,50 +57,45 @@ class Scan2DTest : FreeSpec() {
 
             "from incremental values" - {
 
-                val testScannerInc = Scanner("TestScanner-Inc", true, 45.0, true, 100)
+                val testScannerInc = Scanner("TestScanner-Inc", true, 45.0, false, 100)
 
                 val tsvInc = """
                     id	step size	distance	quality
-                    13	0	1	54
-                    2	1	1.41421356	58
-                    1	1	1	62
+                    13	0	1	1
+                    2	1	1.41421356	2
+                    1	1	1	3
                 """.trimIndent()
 
                 val tsvIncReverse = """
                     id	step size	distance	quality
-                    13	0	1	54
-                    2	1	1.41421356	58
-                    1	1	1	62
+                    13	0	1	1
+                    2	1	1.41421356	2
+                    1	1	1	3
                 """.trimIndent()
 
                 val targetPointCloud = table(
                     headers("Point"),
-                    row(Point(1.0, 0.0, 54)),
-                    row(Point(1.0, -1.0, 58)),
-                    row(Point(0.0, -1.0, 62))
+                    row(Point(1.0, 0.0, 1)),
+                    row(Point(sqrt(2.0), 45.0, 2)),
+                    row(Point(1.0, 90.0, 3))
                 )
 
-                "should be computed" {
-                    val scan = Scan2D.fromTSV(tsvInc, testScannerInc)
-                    scan.pointCloud.size shouldBe 3
-                    scan.pointCloud.asClue {
-                        forAll(targetPointCloud) {
-                            scan.pointCloud shouldContain it
-                        }
-                    }
-                }
-
-                "should be computed, even reverse" {
-                    val scan = Scan2D.fromTSV(tsvIncReverse, testScannerInc)
-                    scan.pointCloud.size shouldBe 3
-                    scan.pointCloud.asClue {
-                        forAll(targetPointCloud) {
-                            scan.pointCloud shouldContain it
+                listOf(
+                    row("normal tsv", Scan2D.fromTSV(tsvInc, testScannerInc)),
+                    row("reverse tsv", Scan2D.fromTSV(tsvIncReverse, testScannerInc))
+                ).map { (method, scan) ->
+                    "should create the pointCloud correctly from $method" {
+                        assertSoftly {
+                            scan.pointCloud.size shouldBe 3
+                            scan.pointCloud.asClue {
+                                forAll(targetPointCloud) {
+                                    scan.pointCloud shouldContain it
+                                }
+                            }
                         }
                     }
                 }
             }
         }
     }
-
-}
+})
