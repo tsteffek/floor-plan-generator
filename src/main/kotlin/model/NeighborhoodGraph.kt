36193b2c2102b54a61cae94585f148343f4c8d2d
tsteffek <thomas.steffek@beuth-hochschule.de>
2019-12-05 21:22:14 +0100

    fix magic number still appearing

--- src/main/kotlin/model/NeighborhoodGraph.kt
+++ src/main/kotlin/model/NeighborhoodGraph.kt
@@ -13,12 +13,15 @@ class NeighborhoodGraph<T : GeometricObject<T>>(
     fun getNeighbors(t: T): Set<T> = map.getValue(t)
 
     companion object {
-        fun <T : GeometricObject<T>> usingBruteForce(objects: List<T>): NeighborhoodGraph<T> {
-            val closestNeighborsPlusIt = 3
+        fun <T : GeometricObject<T>> usingBruteForce(
+            objects: List<T>,
+            closestNeighbors: Int = 2
+        ): NeighborhoodGraph<T> {
             val objectToList = objects.associateWith {
-                objects
+                objects.asSequence()
                     .sortedBy { other -> it.distanceTo(other) }
-                    .take(closestNeighborsPlusIt)
+                    .minus(it)
+                    .take(closestNeighbors)
                     .filter { other -> it !== other }
                     .toSet()
             }
@@ -49,24 +52,6 @@ class NeighborhoodGraph<T : GeometricObject<T>>(
             val halfSize = (points.size * 0.5).toInt()
             val forwardIterator = points.asCyclicSequence(index + 1, index + halfSize).iterator()
             val backwardIterator = points.asCyclicReversed(index - 1, index - halfSize).iterator()
-            tailrec fun computeNextClosestRec(
-                it: Point,
-                closestPoint: Point?,
-                distanceToClosest: Double,
-                iterator: Iterator<Point>
-            ): Point? {
-                if (!iterator.hasNext()) return closestPoint
-                val nextPoint = iterator.next()
-
-                val shortestPossibleDistance = distanceOriginLineToPoint(nextPoint.angle, it)
-                if (distanceToClosest < shortestPossibleDistance || it == nextPoint) return closestPoint
-
-                val distanceToNext = distancePointToPoint(it, nextPoint)
-                return if (distanceToClosest < distanceToNext)
-                    computeNextClosestRec(it, closestPoint, distanceToClosest, iterator)
-                else
-                    computeNextClosestRec(it, nextPoint, distanceToNext, iterator)
-            }
             return setOf(
                 computeNextClosestRec(
                     it, null, Double.POSITIVE_INFINITY, forwardIterator
@@ -76,6 +61,25 @@ class NeighborhoodGraph<T : GeometricObject<T>>(
                 )!!
             )
 
+        }
+
+        private tailrec fun computeNextClosestRec(
+            it: Point,
+            closestPoint: Point?,
+            distanceToClosest: Double,
+            iterator: Iterator<Point>
+        ): Point? {
+            if (!iterator.hasNext()) return closestPoint
+            val nextPoint = iterator.next()
+
+            val shortestPossibleDistance = distanceOriginLineToPoint(nextPoint.angle, it)
+            if (distanceToClosest < shortestPossibleDistance || it == nextPoint) return closestPoint
+
+            val distanceToNext = distancePointToPoint(it, nextPoint)
+            return if (distanceToClosest < distanceToNext)
+                computeNextClosestRec(it, closestPoint, distanceToClosest, iterator)
+            else
+                computeNextClosestRec(it, nextPoint, distanceToNext, iterator)
         }
     }
 }
